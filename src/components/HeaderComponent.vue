07befe56af05d8723d58b5c8cef224bb14897f01
irenk <kuzmina.iren.v@gmail.com>
2023-01-12 16:59:26 +0100

    add styles for header for БЭМ

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,7 @@
         <header>
             <div class="header__head">
                 <img class="logo" src="../assets/imgs/header/header-logo.png" alt="Logo">
-                <div class="header__links-wrap">
+                <div class="header__links-wrap header__links--position">
                     <a class="header__link" href="#">Sale</a>
                     <a class="header__link" href="#">Rent</a>
                 </div>
@@ -20,6 +20,40 @@
                     </button>
                     <div class="header__message">
                         <p>2</p>
+                    </div>
+                </div>
+            </div>
+            <div class="header__banner">
+                <h1 class="header__title">The most convenient real estate directory in Greece!</h1>
+                <div class="header__filter">
+                    <img class="header__filter-img" src="../assets/imgs/section-main/main-img1.svg" alt="img">
+                    <div class="header__filter-wrap">
+                        <button class="header__filter-btn" type="button">
+                            District
+                        </button>
+                        <p class="header__filter-text">In what area?</p>
+                    </div>
+                    <div class="header__filter-line">
+                        </div>
+                    <img class="header__filter-img" src="../assets/imgs/section-main/main-img1.svg" alt="img">
+                    <div class="header__filter-wrap">
+                        <button class="header__filter-btn" type="button">
+                            District
+                        </button>
+                        <p class="header__filter-text">In what area?</p>
+                    </div>
+                    <div class="header__filter-line">
+                        </div>
+                    <img class="header__filter-img" src="../assets/imgs/section-main/main-img1.svg" alt="img">
+                    <div class="header__filter-wrap">
+                        <button class="header__filter-btn" type="button">
+                            District
+                        </button>
+                        <p class="header__filter-text">In what area?</p>
+                    </div>
+                    <div class="header__filter-btn-submit">
+                        <img class="header__filter-btn-img" src="../assets/imgs/section-main/serach-btn.svg" alt="img">
+                        <img class="header__filter-btn-img-mobile" src="../assets/imgs/section-main/mobile2.svg" alt="img">
                     </div>
                 </div>
             </div>
