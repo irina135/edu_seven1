cefd687f36dc1c61d10632f93d746f8735d2e83f
irenk <kuzmina.iren.v@gmail.com>
2023-01-12 18:38:21 +0100

    add style for Search for БЭМ

--- src/components/SearchSection.vue
+++ src/components/SearchSection.vue
@@ -1,27 +1,27 @@
 <template>
     <div>
       <section class="search search--pb seaarch--pt container">
-        <h3>What to search?</h3>   
+        <h3 class="search__title">What to search?</h3>   
         <div class="search__item">
-            <button type="button">
-                <img src="../assets/imgs/search/search1.svg">
+            <button class="search__button" type="button">
+                <img class="search__buttim-img" src="../assets/imgs/search/search1.svg" alt="img">
             </button>
-            <p>House</p>
+            <p class="search__text">House</p>
         </div>
         <div class="search__item">
-            <button class="button--yellow" type="button">
-                <img src="../assets/imgs/search/search2.svg">
+            <button class="search__button search__button--yellow" type="button">
+                <img class="search__buttim-img" src="../assets/imgs/search/search2.svg" alt="img">
             </button>
-            <p>Apartment</p>
+            <p class="search__text">Apartment</p>
         </div>
         <div class="search__item">
-            <button class="button--grey" type="button">
-                <img src="../assets/imgs/search/search3.svg">
+            <button class="search__button search__button--grey" type="button">
+                <img class="search__buttim-img" src="../assets/imgs/search/search3.svg" alt="img">
             </button>
-            <p>Commercial 
+            <p class="search__text">Commercial 
                 Real Estate</p>
         </div>
-    </section>
+        </section>
     </div>
   </template>
   
