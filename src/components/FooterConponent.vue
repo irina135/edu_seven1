dcabd34ffe1717e0beaa2b64c3f1a2cd3818bda1
irenk <kuzmina.iren.v@gmail.com>
2023-01-12 18:54:34 +0100

    add style dor footer for БЭМ

--- src/components/FooterConponent.vue
+++ src/components/FooterConponent.vue
@@ -1,28 +1,28 @@
 <template>
     <div>
         <footer class="footer footer--pt footer--pb">
-        <div class="footer__logo">
-            <img src="../assets/imgs/footer/footer-logo.svg">
-            <p>The most convenient real estate directory in Greece!</p>
-        </div>
-        <div class="footer__buy">
-            <h4>I want to buy/rent</h4>
-            <ul>
-                <li><a href="#">House</a></li>
-                <li><a href="#">Apartment</a></li>
-                <li><a href="#">Commercial Real Estate</a></li>
-                <li><a href="#">Housing from the developer</a></li>
-            </ul>
-        </div>
-        <div class="footer__sell">
-            <h4>How to sell/rent real estate?</h4>
-            <ul>
-                <li><a href="#">I am the owner</a></li>
-                <li><a href="">I am a realtor</a></li>
-                <li><a href="#">I am a developer</a></li>
-            </ul>
-        </div>
-    </footer>
+            <div class="footer__logo">
+                <img class="logo footer__img" src="../assets/imgs/footer/footer-logo.svg" alt="Logo">
+                <p class="footer__text">The most convenient real estate directory in Greece!</p>
+            </div>
+            <div class="footer__buy">
+                <h4 class="footer__title">I want to buy/rent</h4>
+                <ul>
+                    <li><a class="footer__link" href="#">House</a></li>
+                    <li><a class="footer__link" href="#">Apartment</a></li>
+                    <li><a class="footer__link" href="#">Commercial Real Estate</a></li>
+                    <li><a class="footer__link" href="#">Housing from the developer</a></li>
+                </ul>
+            </div>
+            <div class="footer__sell">
+                <h4>How to sell/rent real estate?</h4>
+                <ul>
+                    <li><a class="footer__link" href="#">I am the owner</a></li>
+                    <li><a class="footer__link" href="">I am a realtor</a></li>
+                    <li><a class="footer__link" href="#">I am a developer</a></li>
+                </ul>
+            </div>
+        </footer>
     </div>
   </template>
   
